8df51b2206d88f9b5fea7f70a98b3d393b4bd6e9
Rui000 <49097363+Rui000@users.noreply.github.com>
2024-04-18 13:55:53 +0200

    change the icot

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ document.getElementById('ExKMC').addEventListener('click', function() {
 });
 
 document.getElementById('ICOT').addEventListener('click', function() {
-    document.getElementById('ExKMC_result').style.display = 'none';
-    document.getElementById('ICOT_result').style.display = 'block';
-    document.getElementById('MPC_result').style.display = 'none';
-    document.getElementById('ExShallow_result').style.display = 'none';
-    document.getElementById('K-means+CART_result').style.display = 'none';
+    fetch('ICOT.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('ExKMC_result').style.display = 'none';
+            document.getElementById('ICOT').style.display = 'block';
+            document.getElementById('ICOT').innerHTML = html;
+            document.getElementById('MPC_result').style.display = 'none';
+            document.getElementById('ExShallow_result').style.display = 'none';
+            document.getElementById('K-means+CART_result').style.display = 'none';
+        })
+        .catch(error => console.error('Error fetching result2.html:', error));
 });
 
 document.getElementById('MPC').addEventListener('click', function() {

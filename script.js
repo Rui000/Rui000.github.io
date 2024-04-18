f8c5ddb9f2d86851e7ad73423e2b6044e2b30531
Rui000 <49097363+Rui000@users.noreply.github.com>
2024-04-18 13:18:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
-document.getElementById('button1').addEventListener('click', function() {
-    document.getElementById('result1').style.display = 'block';
-    document.getElementById('result2').style.display = 'none';
+ExKMC_result" class="result">
+                <!-- Content for result 1 -->
+                <img src="ExKMC_result.jpg" alt="Result 1 Image 1">
+            </div>
+            <div id="ICOT_result" class="result" style="display: none;">
+                <!-- Content for result 2 -->
+                <img src="ICOT_result.jpg" alt="Result 2 Image 1">  
+            </div>
+            <div id="MPC_result" class="result" style="display: none;">
+                <!-- Content for result 2 -->
+                <img src="MPC_result.jpg" alt="Result 3 Image 1">  
+            </div>
+            <div id="ExShallow_result" class="result" style="display: none;">
+                <!-- Content for result 2 -->
+                <img src="ExShallow_result.jpg" alt="Result 4 Image 1">  
+            </div>
+            <div id="K-means+CART_result" class="result" style="display: none;">
+                <!-- Content for result 2 -->
+                <img src="K-means+CART.jpg" alt="Result 5 Image 1">  
+
+
+document.getElementById('ExKMC').addEventListener('click', function() {
+    document.getElementById('ExKMC_result').style.display = 'block';
+    document.getElementById('ICOT_result').style.display = 'none';
+    document.getElementById('MPC_result').style.display = 'none';
+    document.getElementById('ExShallow_result').style.display = 'none';
+    document.getElementById('K-means+CART_result').style.display = 'none';
 });
 
-document.getElementById('button2').addEventListener('click', function() {
-    document.getElementById('result1').style.display = 'none';
-    document.getElementById('result2').style.display = 'block';
+document.getElementById('ICOT').addEventListener('click', function() {
+    document.getElementById('ExKMC_result').style.display = 'none';
+    document.getElementById('ICOT_result').style.display = 'block';
+    document.getElementById('MPC_result').style.display = 'none';
+    document.getElementById('ExShallow_result').style.display = 'none';
+    document.getElementById('K-means+CART_result').style.display = 'none';
+});
+
+document.getElementById('MPC').addEventListener('click', function() {
+    document.getElementById('ExKMC_result').style.display = 'none';
+    document.getElementById('ICOT_result').style.display = 'none';
+    document.getElementById('MPC_result').style.display = 'block';
+    document.getElementById('ExShallow_result').style.display = 'none';
+    document.getElementById('K-means+CART_result').style.display = 'none';
+});
+
+document.getElementById('ExShallow').addEventListener('click', function() {
+    document.getElementById('ExKMC_result').style.display = 'none';
+    document.getElementById('ICOT_result').style.display = 'none';
+    document.getElementById('MPC_result').style.display = 'none';
+    document.getElementById('ExShallow_result').style.display = 'block';
+    document.getElementById('K-means+CART_result').style.display = 'none';
+});
+
+document.getElementById('K-means+CART').addEventListener('click', function() {
+    document.getElementById('ExKMC_result').style.display = 'none';
+    document.getElementById('ICOT_result').style.display = 'none';
+    document.getElementById('MPC_result').style.display = 'none';
+    document.getElementById('ExShallow_result').style.display = 'none';
+    document.getElementById('K-means+CART_result').style.display = 'block';
 });

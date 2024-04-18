30d38c3672e9d89d4df1fd30ea0542c72c6bbece
Rui000 <49097363+Rui000@users.noreply.github.com>
2024-04-18 13:18:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-ExKMC_result" class="result">
-                <!-- Content for result 1 -->
-                <img src="ExKMC_result.jpg" alt="Result 1 Image 1">
-            </div>
-            <div id="ICOT_result" class="result" style="display: none;">
-                <!-- Content for result 2 -->
-                <img src="ICOT_result.jpg" alt="Result 2 Image 1">  
-            </div>
-            <div id="MPC_result" class="result" style="display: none;">
-                <!-- Content for result 2 -->
-                <img src="MPC_result.jpg" alt="Result 3 Image 1">  
-            </div>
-            <div id="ExShallow_result" class="result" style="display: none;">
-                <!-- Content for result 2 -->
-                <img src="ExShallow_result.jpg" alt="Result 4 Image 1">  
-            </div>
-            <div id="K-means+CART_result" class="result" style="display: none;">
-                <!-- Content for result 2 -->
-                <img src="K-means+CART.jpg" alt="Result 5 Image 1">  
-
 
 document.getElementById('ExKMC').addEventListener('click', function() {
     document.getElementById('ExKMC_result').style.display = 'block';

5a51438382c85015a162ac1465bdef3ba84bb27e
Rui000 <49097363+Rui000@users.noreply.github.com>
2024-04-18 16:03:41 +0200

    change

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById('ExKMC').addEventListener('click', function() {
     document.getElementById('MPC_result').style.display = 'none';
     document.getElementById('ExShallow_result').style.display = 'none';
     document.getElementById('K-means+CART_result').style.display = 'none';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'none';
-    document.getElementById('precision').style.display = 'none';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'none';
+    document.getElementById('Precision').style.display = 'none';
 });
 
 document.getElementById('ICOT').addEventListener('click', function() {
@@ -16,9 +16,9 @@ document.getElementById('ICOT').addEventListener('click', function() {
     document.getElementById('MPC_result').style.display = 'none';
     document.getElementById('ExShallow_result').style.display = 'none';
     document.getElementById('K-means+CART_result').style.display = 'none';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'none';
-    document.getElementById('precision').style.display = 'none';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'none';
+    document.getElementById('Precision').style.display = 'none';
 });
 
 document.getElementById('MPC').addEventListener('click', function() {
@@ -27,8 +27,8 @@ document.getElementById('MPC').addEventListener('click', function() {
     document.getElementById('MPC_result').style.display = 'block';
     document.getElementById('ExShallow_result').style.display = 'none';
     document.getElementById('K-means+CART_result').style.display = 'none';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'none';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'none';
     document.getElementById('precision').style.display = 'none';
 });
 
@@ -38,9 +38,9 @@ document.getElementById('ExShallow').addEventListener('click', function() {
     document.getElementById('MPC_result').style.display = 'none';
     document.getElementById('ExShallow_result').style.display = 'block';
     document.getElementById('K-means+CART_result').style.display = 'none';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'none';
-    document.getElementById('precision').style.display = 'none';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'none';
+    document.getElementById('Precision').style.display = 'none';
 });
 
 document.getElementById('K-means+CART').addEventListener('click', function() {
@@ -49,38 +49,38 @@ document.getElementById('K-means+CART').addEventListener('click', function() {
     document.getElementById('MPC_result').style.display = 'none';
     document.getElementById('ExShallow_result').style.display = 'none';
     document.getElementById('K-means+CART_result').style.display = 'block';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'none';
-    document.getElementById('precision').style.display = 'none';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'none';
+    document.getElementById('Precision').style.display = 'none';
 });
 
-document.getElementById('recall').addEventListener('click', function() {
+document.getElementById('Recall').addEventListener('click', function() {
     document.getElementById('ExKMC_result').style.display = 'none';
     document.getElementById('ICOT_result').style.display = 'none';
     document.getElementById('MPC_result').style.display = 'none';
     document.getElementById('ExShallow_result').style.display = 'none';
     document.getElementById('K-means+CART_result').style.display = 'none';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'none';
-    document.getElementById('recall').style.display = 'block';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'none';
+    document.getElementById('Precision').style.display = 'block';
 });
-document.getElementById('f1').addEventListener('click', function() {
+document.getElementById('F1-score').addEventListener('click', function() {
     document.getElementById('ExKMC_result').style.display = 'none';
     document.getElementById('ICOT_result').style.display = 'none';
     document.getElementById('MPC_result').style.display = 'none';
     document.getElementById('ExShallow_result').style.display = 'none';
     document.getElementById('K-means+CART_result').style.display = 'none';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'block';
-    document.getElementById('precision').style.display = 'none';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'block';
+    document.getElementById('Precision').style.display = 'none';
 });
-document.getElementById('precision').addEventListener('click', function() {
+document.getElementById('Precision').addEventListener('click', function() {
     document.getElementById('ExKMC_result').style.display = 'none';
     document.getElementById('ICOT_result').style.display = 'none';
     document.getElementById('MPC_result').style.display = 'none';
     document.getElementById('ExShallow_result').style.display = 'none';
     document.getElementById('K-means+CART_result').style.display = 'none';
-    document.getElementById('recall').style.display = 'none';
-    document.getElementById('f1').style.display = 'none';
-    document.getElementById('precision').style.display = 'block';
+    document.getElementById('Recall').style.display = 'none';
+    document.getElementById('F1-score').style.display = 'none';
+    document.getElementById('Precision').style.display = 'block';
 });
